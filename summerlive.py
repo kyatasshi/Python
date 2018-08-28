e745fa5f17f0fcd666107689595e2a4330512f7a
kyatasshi <atsushi_kataoka@albert2005.co.jp>
2018-08-28 17:14:23 +0900

    if mainなし

--- summerlive.py
+++ summerlive.py
@@ -7,9 +7,9 @@ def first_saturday(weekday):
         saturday = 1 + (5 - weekday)
     return saturday
 
-if __name__ == '__name__':
-    today_year = date.today().year
+#if __name__ == '__name__':
+today_year = date.today().year
 
-    for next_year in range(10):
-        august_firstday = date(today_year + next_year, 8, 1)
-        print('{0.year}/{0.month}/{1}'.format(august_firstday, first_saturday(august_firstday.weekday())))
+for next_year in range(10):
+    august_firstday = date(today_year + next_year, 8, 1)
+    print('{0.year}/{0.month}/{1}'.format(august_firstday, first_saturday(august_firstday.weekday())))
