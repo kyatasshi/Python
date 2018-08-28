5868d59bfae912a8ae3d5343a678a6469c78adbe
kyatasshi <atsushi_kataoka@albert2005.co.jp>
2018-08-28 17:06:19 +0900

    summerlive

---
+++ summerlive.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+def first_saturday(weekday):
+    if weekday == 6:
+        saturday = 8 + (5 - weekday)
+    else:
+        saturday = 1 + (5 - weekday)
+    return saturday
+
+if __name__ == '__name__':
+    today_year = date.today().year
+
+    for next_year in range(10):
+        august_firstday = date.datetime(today_year + next_year, 8, 1)
+        print('{0.year}/{0.month}/{1}'.format(august_firstday, first_saturday(august_firstday.weekday())))
